413597eaeb74f27bce728ae45c15ae37e111f42c
Владислав <106807250+Destian1995@users.noreply.github.com>
2023-08-24 20:47:16 +0400

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,4 @@
-# output "internal_ip_address_vm_instance_ForEach" {
-#   value = values(yandex_compute_instance.ForEach).*.network_interface.0.ip_address
-# }
 
-# output "external_ip_address_vm_instance_ForEach" {
-#   value = values(yandex_compute_instance.ForEach).*.network_interface.0.nat_ip_address
-# }
 
 output "internal_ip_address_vm_instance_worker" {
   value = yandex_compute_instance.worker.*.network_interface.0.ip_address
